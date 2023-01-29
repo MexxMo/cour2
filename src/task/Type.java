d2d5faa3808649c2db5297307eb20a4802277d7d
Ildar <mexx0174@gmail.com>
2023-01-29 21:42:51 +0500

    Готово

--- src/task/Type.java
+++ src/task/Type.java
@@ -1,7 +1,15 @@
 package task;
 
 public enum Type {
-    WORK,
-    PERSONAL;
+    WORK("Рабочая"),
+    PERSONAL("Личная");
+    private final String translate;
+    Type(String translate){
+        this.translate = translate;
+    }
 
+    @Override
+    public String toString() {
+        return translate;
+    }
 }
